88da6671278afde5933ff95a782882ec5d7458a6
fara <farid34759@mail.ru>
2025-01-19 15:53:25 +0700

    catolog_view

--- rollers/main/views.py
+++ rollers/main/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DatailView
+from .models import ClothingItem, Category, Size
+from django.db.models import Q
 
-# Create your views here.
+
+class CatalogView(ListView):
+    model = ClothingItem
+    template_name = 'main/product/list.html'
+    context_object_name = 'cloting_items'
+    
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_slugs = self.request.GET.getlist('category')
+        size_names = self.request.GET.getlist('size')
+        min_price = self.request.GET.get('min_price')
+        max_price = self.request.GET.get('max_price')
+        
+        if category_slugs:
+            queryset = queryset.filter(category__slug__in=category_slugs)
+            
+        if size_names:
+            queryset = queryset.filter(
+                Q(sizes__name__in=size_names) & Q(size__clotingitemsize_available=True)
+            )
+
+        if min_price:
+            queryset = queryset.filter(price__gte=min_price)
+            
+        if max_price:
+            queryset = queryset.filter(price__lte=max_price)
+
+        return queryset
+    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['sizes'] = Size.objects.all()
+        context['selected_categories'] = self.request.GET.getlist('category')
+        context['selected_sizes'] = self.request.GET.getlist('size')
+        context['min_price'] = self.request.GET.get('min_prict', '')
+        context['max_price'] = self.request.GET.get('max_prict', '')
+
+        return context 
+    
+
+class ClothingItemDetailView(DatailView):
+    model = ClothingItem
+    template_name = 'main/product/detail.html'
+    context_object_name = 'clothing_item'
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+    
